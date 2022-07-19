0553bd0a37e3dcbea89189cd477efa8d4beb6186
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-07-18 21:23:52 -0300

    novo documento

---
+++ Aulas/18_07_22/index.js
@@ -0,0 +1,19 @@
+// ----- TIPOS de VARIÁVEIS ------
+
+// CONST - variável imutável
+//const pi = 3.14
+//console.log (pi)
+
+
+
+//VAR - variável de uso geral
+//var veículo = carro
+//console.log()
+
+
+
+//LET - variável descartável
+//function nome (){
+//    let = josé
+//}
+//console.log(nome)
