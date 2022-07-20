7086e9208dec8587c2e29724e6550b6a953c9e42
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-07-20 20:05:31 -0300

    chore: instanciando user

--- Aulas/20_07_22/index.js
+++ Aulas/20_07_22/index.js
@@ -12,3 +12,25 @@ class Pessoa{
 
 const alunoSenai = new Pessoa ("fernando", 24, "000.000.000-00")
 console.log(alunoSenai)
+
+
+
+//Atividade em aula com alunos
+class User{
+    login;
+    senha;
+    dataNascimento;
+    nome;
+    
+    constructor(login, senha, dataNascimento, nome){
+       this.login = login;
+       this.senha = senha;
+       this.dataNascimento = dataNascimento;
+       this.nome = nome;
+    }
+    cadastrar(){}
+    editar(){}
+    postar(){}
+   inserirAvatar(){}
+}
+const usuario = new User("UserClamed", "clamed", "20/07/2022", "Clamedinho")
