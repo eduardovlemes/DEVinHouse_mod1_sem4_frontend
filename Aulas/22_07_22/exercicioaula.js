c81c09b8da3695ee3e8d1fa49849cfa0cbf284aa
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-07-22 19:58:08 -0300

    fix: ajustes na função

--- Aulas/22_07_22/exercicioaula.js
+++ Aulas/22_07_22/exercicioaula.js
@@ -1,11 +1,9 @@
 const array = [1,22,31,40,3,5]
-let valorInicial = 0
-let maiorValor = array.reduce(
-    function(valorAnterior, valorAtual) {
+let maiorValor = array.reduce(function(valorAnterior, valorAtual) {
         if (valorAtual > valorAnterior){
             valorAnterior = valorAtual
         }
     return valorAnterior
-    },
+    }, 0 //Esse zero serve para informar o valor incial, pois se não for definido será undefined.
 );
   console.log(maiorValor)
