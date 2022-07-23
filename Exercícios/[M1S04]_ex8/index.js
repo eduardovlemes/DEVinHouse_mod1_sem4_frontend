c7d1c36da96ab3b245785f09c2b01e0f8d6ebdb0
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-07-22 22:09:13 -0300

    chore: ex4

---
+++ Exercícios/[M1S04]_ex8/index.js
@@ -0,0 +1,36 @@
+class Series {
+  nome;
+  anoLancamento;
+  qttTemporada;
+
+  constructor(nome, anoLancamento, qttTemporada) {
+    this.nome = nome;
+    this.anoLancamento = anoLancamento;
+    this.qttTemporada = qttTemporada;
+  }
+}
+
+let maisAntiga;
+let nomeAntigo;
+
+for (let i = 0; i <= 3; i++) {
+  let nome = prompt("digite o nome da série:");
+  let anoLancamento = Number(prompt("digite o ano de lançamento da série:"));
+  let qttTemporada = Number(
+    prompt("digite a quantidade de temporadas da série:")
+  );
+
+  const serie = new Series(nome, anoLancamento, qttTemporada);
+  if (i == 0) {
+    maisAntiga = serie.anoLancamento;
+  } else {
+    if (serie.anoLancamento < maisAntiga) {
+      maisAntiga = serie.anoLancamento;
+      nomeAntigo = serie.nome;
+    }
+  }
+}
+console.log(maisAntiga);
+console.log(nomeAntigo);
+let tagH1 = document.getElementById("h1");
+tagH1.innerHTML += nomeAntigo;
