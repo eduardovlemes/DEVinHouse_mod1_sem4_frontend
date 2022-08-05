7af3bd533db87b88e276b68c3d8bd1dfcfa570f6
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-08-05 10:00:23 -0300

    feat: inserindo sort ()

---
+++ Exercícios/[M1S04]_ex5/index.js
@@ -0,0 +1,18 @@
+/* Crie um código javascript que irá juntar os dois arrays abaixo e coloque esse array concatenado em outra variável:
+let array1 = [1,3,4,5,3,7];
+let array2 = [1,2,6,4,8,5];
+Após fazer a junção remova os itens que se repetem usando o .includes ou o .filter
+Após isso ordene por ordem crescente. */
+
+let array1 = [1,3,4,5,3,7];
+let array2 = [1,2,6,4,8,5];
+
+let newArray = array1.concat(array2)
+
+const separaNumerosRepetidos = newArray.filter(function(itemAnterior, itemAtual) {
+        return newArray.indexOf(itemAnterior) == itemAtual
+    }
+)
+
+const novoArrayEmOrdemCrescente = separaNumerosRepetidos.sort()
+console.log(novoArrayEmOrdemCrescente)
